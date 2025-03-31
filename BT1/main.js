3a063dd7c84a8c81242c508aae703dbedbe686b3
NguyenGiaHuy-Qi <trueperfect302@gmail.com>
2025-04-01 01:03:15 +0700

    Update main.js

--- BT1/main.js
+++ BT1/main.js
@@ -9,7 +9,7 @@ if(input == 0){
     alert("Dữ liệu không hợp lệ");
 }else{
     for(let i = 0;i < arr.length;i++){
-        if(arr[i] > 10){
+        if(arr[i] >= 10){
             console.log(arr[i]);
         }
     }
